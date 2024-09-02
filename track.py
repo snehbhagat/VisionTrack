9708cc1d05af95b1617693ab9024d45dfd31d8df
Sneh Bhagat <snehbhagat12@gmail.com>
2024-09-02 21:49:49 +0530

    Adding video frame for our project

--- track.py
+++ track.py
@@ -20,3 +20,27 @@ def selectROI(event, x, y, flags, param):
         cv2.circle(frame, (x, y), 4, (0, 255, 0), 2)
         cv2.imshow("frame", frame)
 
+def main():
+    # construct the argument parse and parse the arguments
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-v", "--video",
+    help = "path to the (optional) video file")
+    args = vars(ap.parse_args())
+    # grab the reference to the current frame, list of ROI
+    # points and whether or not it is ROI selection mode
+    global frame, roiPts, inputMode
+    # if the video path was not supplied, grab the reference to the
+    # camera
+    if not args.get("video", False):
+        camera = cv2.VideoCapture(0)
+        # otherwise, load the video
+    else:
+        camera = cv2.VideoCapture(args["video"])
+        # setup the mouse callback
+        cv2.namedWindow("frame")
+        cv2.setMouseCallback("frame", selectROI)
+        # initialize the termination criteria for cam shift, indicating
+        # a maximum of ten iterations or movement by a least one pixel
+        # along with the bounding box of the ROI
+        termination = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 1)
+        roiBox = None
